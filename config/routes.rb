72a8867910fda9c2922ef78c00ae09cc4ba6b05c
AsheDuran <bady.0222.monk@gmail.com>
2022-11-05 14:11:49 +0000

    deviseインストールからviewの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  devise_for :end_users, skip: [:passwords], controllers: {#skipオプションで不要なルーティングを削除
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }#生成したコントローラーがどこに存在するかを記述
+
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {#skipオプションで不要なルーティングを削除
+    sessions: "admin/sessions"
+  }#生成したコントローラーがどこに存在するかを記述
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
