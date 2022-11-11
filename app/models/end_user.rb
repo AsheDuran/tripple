34523bc6dbe7d517f0b2192c888c21bb6eb773cc
AsheDuran <bady.0222.monk@gmail.com>
2022-11-11 11:23:27 +0000

    サイト由来修正

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -13,8 +13,8 @@ class EndUser < ApplicationRecord
 
   def get_profile_image(width, height)
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.png')
-      profile_image.attach(io: File.open(file_path), filename: 'no_image.png', content_type: 'image/png')
+      file_path = Rails.root.join('app/assets/images/no_profile_image.png')
+      profile_image.attach(io: File.open(file_path), filename: 'no_profile_image.png', content_type: 'image/png')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
