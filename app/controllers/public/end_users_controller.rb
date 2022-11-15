ea1a01db4277a74acd5a069df2b2bce25efcff61
AsheDuran <bady.0222.monk@gmail.com>
2022-11-15 12:15:50 +0000

    検索機能修正

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -3,14 +3,14 @@ class Public::EndUsersController < ApplicationController
  def show
    @end_user = EndUser.find(params[:id])
    #byebug
-   if @end_user.id == current_end_user.id#自分やったら全投稿出る
+   if @end_user.id == current_end_user.id#自分だったら、全投稿が表示される。
      @spots = @end_user.spots
    else
      @spots = @end_user.spots.published
    end
  end
 
- def my_page#showでcurrent_end_user使ったら、ややこしかったので、こっちに記述した。
+ def my_page#showでcurrent_end_userを使用したら、ややこしかったので、こっちに記述した。
    @end_user = current_end_user
    @spots = @end_user.spots
    render :show#本当はshowページに記載するため、render先はshow
