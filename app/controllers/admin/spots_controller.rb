3a243dde3fb5289683e2c3cd5fc93a999f4bcebb
AsheDuran <bady.0222.monk@gmail.com>
2022-12-21 09:45:44 +0000

    管理者側検索機能修正

--- app/controllers/admin/spots_controller.rb
+++ app/controllers/admin/spots_controller.rb
@@ -17,13 +17,13 @@ class Admin::SpotsController < ApplicationController
   end
 
   def search
-    @spots = Spot.where(prefecture_id: params[:prefecture_id]).
-                  page(params[:page]).or (Spot.where(genre_id: params[:genre_id])).
-                  page(params[:page]).or (Spot.where(name: params[:name])).
-                  page(params[:page])
+    query = Spot.published
+    query = query.where(prefecture_id: params[:prefecture_id]) if params[:prefecture_id].present?
+    query = query.where(genre_id: params[:genre_id]) if params[:genre_id].present?
+    query = query.where("name LIKE ?", "%#{params[:name]}%") if params[:name].present?
+    @spots = query.page(params[:page])
     render :index
   end
-
 
   private
 
